3f653e34e3ed001697c05abd32f0c390ccf83355
Dharmil shah <115447681+Dharmil03@users.noreply.github.com>
2024-04-21 12:29:51 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Blogs from '../components/Blogs'
 import Header from '../components/Header'
 import Pagination from '../components/Pagination'
-
+//uses all components 
 const Home = () => {
   return (
     <div>
